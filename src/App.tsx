8438ed7fae5fb6e326fab1b74e3392e3a4881cde
zaaine <zaaine@livefr>
2025-09-17 15:18:20 +0200

    Feature : Create components Header and Layout for Routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
-
-import { Route, Routes } from "react-router";
-import NotFoundPage from "./pages/notFoundPage";
-
+import { Route, Routes } from 'react-router';
+import Layout from './components/Layout/Layout';
+import NotFoundPage from './pages/notFoundPage';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<p>Home</p>} />
-        <Route path="/challenges" element={<p>Challenges</p>} />
-        <Route
-          path="/challenges/:challengeId"
-          element={<p>Challenge Détails</p>}
-        />
-        <Route path="*" element={<NotFoundPage />} />
+        <Route path="/" element={<Layout />}>
+        <Route path='/challenges' element={ <p>Challenges</p>} />
+        <Route path='/challenges/:challengeId' element={ <p>Challenge</p>} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Route>
       </Routes>
     </>
   )
