214d18df7fff93f21aca5901292e75ed5d0663f6
zaaine <zaaine@livefr>
2025-09-17 15:18:20 +0200

    Feature : Create components Header and Layout for Routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { Route, Routes } from 'react-router';
+import Layout from './components/Layout/Layout';
 import NotFoundPage from './pages/notFoundPage';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={ <p>Home</p>} />
+        <Route path="/" element={<Layout />}>
         <Route path='/challenges' element={ <p>Challenges</p>} />
         <Route path='/challenges/:challengeId' element={ <p>Challenge</p>} />
         <Route path='*' element={<NotFoundPage />} />
+      </Route>
       </Routes>
     </>
   );
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,59 @@
+import { AppBar, Button, Toolbar, useMediaQuery } from '@mui/material';
+import Avatar from '../../assets/avatar.svg';
+import Logo from '../../assets/logo_GamerChallenges.svg';
+import LogoSmall from '../../assets/logogram.svg';
+
+export default function Header() {
+  const isSmallScreen = useMediaQuery('(max-width:600px)');
+
+  return (
+    <AppBar
+      position="static"
+      sx={{ backgroundColor: '#333333', color: '#EBEBFF' }}
+    >
+      <Toolbar className="w-full max-w-7xl mx-auto flex justify-between items-center">
+        <img
+          src={isSmallScreen ? LogoSmall : Logo}
+          alt="Logo Gamer Challenges"
+          className="h-10"
+        />
+
+        {isSmallScreen ? (
+          <Button
+            size="small"
+            sx={{
+              backgroundColor: '#8585FF',
+              color: '#EBEBFF',
+              borderRadius: '9999px',
+              padding: '8px 20px',
+              '&:hover': {
+                backgroundColor: '#6B6BFF',
+              },
+            }}
+          >
+            <img
+              src={Avatar}
+              alt="Avatar"
+              className="h-10"
+            />
+          </Button>
+        ) : (
+          <Button
+            size="small"
+            sx={{
+              backgroundColor: '#8585FF',
+              color: '#EBEBFF',
+              borderRadius: '9999px',
+              padding: '8px 20px',
+              '&:hover': {
+                backgroundColor: '#6B6BFF',
+              },
+            }}
+          >
+            CONNECTION
+          </Button>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+}
