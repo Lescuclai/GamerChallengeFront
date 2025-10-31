90b42aa26b7d970d909849a7e903695a913431c6
Romain Larrouy <romain.larrouy@viacesi.Fr>
2025-10-31 14:06:59 +0100

    fix: deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,13 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import tailwindcss from "@tailwindcss/vite"
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist",
+  },
+  base: mode === "production" ? "/GamerChallengeFront/" : "./",
   server: {
     port: 3000,
   },
-})
+}))
